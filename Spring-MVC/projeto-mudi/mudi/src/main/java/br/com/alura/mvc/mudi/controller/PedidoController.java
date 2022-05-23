462dbd62f896ffdd11d2c6fcbd9844afa913a5b2
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2022-05-23 20:00:14 -0300

    Add Bean Validation

--- Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +21,15 @@ public class PedidoController {
     private PedidoRepository pedidoRepository;
    
     @GetMapping("formulario")
-    public String formulario() {
+    public String formulario(PedidoDto pedidoDto) {
         return "pedido/formulario";
     }
 
     @PostMapping("novo")
-    public String novo(PedidoDto pedidoDto) {
-
+    public String novo(@Valid PedidoDto pedidoDto , BindingResult result) {
+        if (result.hasErrors()) {
+            return "pedido/formulario";
+        }
         Pedido pedido = pedidoDto.toPedido();
         pedidoRepository.findAll();
         pedidoRepository.save(pedido);
