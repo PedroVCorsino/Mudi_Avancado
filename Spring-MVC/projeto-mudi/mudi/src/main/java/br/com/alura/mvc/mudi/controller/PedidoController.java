4b003de0f7d0d287d5cd123ef7de8ea5b859e838
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2022-05-25 10:43:34 -0300

    ALterando redirecionamento ao salvar novo pedido

--- Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -34,6 +34,6 @@ public class PedidoController {
         pedidoRepository.findAll();
         pedidoRepository.save(pedido);
         
-        return "pedido/formulario";
+        return "redirect:/home";
     }
 }
