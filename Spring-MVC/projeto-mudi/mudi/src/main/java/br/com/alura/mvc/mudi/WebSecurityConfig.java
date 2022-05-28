72913ffddded010e60853ecd150317cab4bbcb97
Pedro Vinicius <pedroviniciuscorsino@gmail.com>
2022-05-28 17:55:00 -0300

    Usando JDBC Authentication para guardar usuarios no banco de dados

--- Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ Spring-MVC/projeto-mudi/mudi/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package br.com.alura.mvc.mudi;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -21,21 +30,41 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
                 .formLogin(form -> form
 					.loginPage("/login")
+					.defaultSuccessUrl("/home", true)
 					.permitAll()
 				)
 				.logout(logout -> logout.logoutUrl("/logout"));
 	}
 
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("pedro")
-				.password("pedro")
-				.roles("ADM")
-				.build();
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		// UserDetails user =
+		// 	 User.builder()
+		// 		.username("judith")
+		// 		.password(encoder.encode("judith"))
+		// 		.roles("ADM")
+		// 		.build();
 
-		return new InMemoryUserDetailsManager(user);
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder)
+			// .withUser(user)
+		;
 	}
+
+	// Configuração de autenticação do Spring Security
+	// @Bean
+	// @Override
+	// public UserDetailsService userDetailsService() {
+	// 	UserDetails user =
+	// 		 User.withDefaultPasswordEncoder()
+	// 			.username("pedro")
+	// 			.password("pedro")
+	// 			.roles("ADM")
+	// 			.build();
+
+	// 	return new InMemoryUserDetailsManager(user);
+	// }
 }
